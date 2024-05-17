23e90a673d473c5f4ddfcf6c4e19c53e074386ba
Quinn van Huizen <153827801+Quinn34@users.noreply.github.com>
2024-05-17 13:24:52 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/main.css" >
+</head>
+<header>
+    <div class="nav">
+        <div class="left-75">
+            <div class="name">
+                <img src="/assets/north travel logo.png" class="kook">
+            </div>
+        </div>
+        <div class="right-20">
+            <div class="buttons">
+            <a href="#" class="Items">Home</a>     
+            <a href="#" class= "Items">Over ons</a>
+            <a href="#" class= "Items">Reizen</a>     
+            <a href="#" class="Items">Contact</a>     
+        </div>
+    </div>
+    <div class="right-5">
+        <div class="buttons">
+            <a href="#" class="Items">Login</a>   
+        </div>
+    </div>
+    </div>
+    <body>
+    <div class="slider">
+        <div class="all-images">
+
+        </div>
+    </div>
+    <div class="container3">
+        <div class="stay">
+            <p>Find your next stay</p>
+        </div>
+        <div class="find">
+            <p>Search low prices on hotels, homes and much more...</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="forms">
+            <div class="little-box">
+                <img src="/assets/bed.png"class="bed">
+                <form>
+                    <input type="text" placeholder="  Where are you going?" class="where">
+                </form>            
+            </div>
+            <div class="little-box">
+            <img src="/assets/cal.png"class="bed">
+                <form>njogrniogeniegwtnip
+                    <input type="text" placeholder="Check-in date -- Check-out date" class="where">
+                </form>      
+            </div>
+            <div class="little-box">
+            <img src="/assets/person.png"class="bed">
+                <form>
+                    <input type="text" placeholder="0 adults - 0 childeren - 0 rooms" class="where">
+                </form>      
+            </div>
+            <div class="smallerbox">
+                <input type="submit" name="submit" value='Search' class="smallerbox"/>
+            </div>
+        </div>
+        <div class="checkbox">
+            <input type="checkbox" class="check">
+            <p>I'm looking for flights</p>
+        </div>
+    </div>
+    <div class="row1">
+        <div class="box">
+            <img src="/assets/chinatown.jpeg" class="img1">
+        </div>
+        <div class="box">
+        <img src="/assets/chinatown1.jpeg" class="img1">
+        </div>
+        <div class="box">
+        <img src="/assets/scania.jpeg" class="img1">
+        </div>
+    </div>
+    <script src="js/main.jsx"></script>
+    </body>
+</header>
