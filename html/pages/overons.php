34be528f078dca6715c56b89df713c76bde9d382
Quinn34 <1207398@student.roc-nijmegen.nl>
2024-05-31 14:41:46 +0200

    begin over ons page

--- html/pages/overons.php
+++ html/pages/overons.php
@@ -27,3 +27,17 @@
         </div>
     </div>
     </div>
+    <body>
+        <div class="container-text">
+            <div class="mid-text1">
+                <p>Over Ons</p>
+                <p>Wij zijn drie studenten aan het ROC Nijmegen, waar we de opleiding Software Development volgen. Voor ons schoolproject in periode 4 ontwikkelen we een boekingssite voor vakanties.
+                Ons Team: </p>
+                <p>Luuk (17 jaar) - Woont in Wijchen. Luuk heeft veel tijd gestoken in het ontwerp van de site en zorgt ervoor dat het er geweldig uitziet.</p>
+                <p>Quinn (18 jaar) - Woont in Beuningen. Quinn werkt vooral aan de front-end en een beetje aan de back-end van de site.</p>
+                <p>Jop (17 jaar) - Woont in Nijmegen. Jop werkt aan zowel de front-end als de back-end, en zorgt ervoor dat alles goed samenwerkt.</p>
+                <p>Ons doel is om een gebruiksvriendelijke boekingssite te maken die het boeken van vakanties eenvoudig en leuk maakt. 
+                <p>We zijn enthousiast over dit project en kijken ernaar uit om onze vaardigheden in de praktijk te brengen.</p>
+            </div>
+        </div>
+    </body>
