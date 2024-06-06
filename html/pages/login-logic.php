149b4edfc2630f62cf584dc22609af5191012295
Quinn34 <1207398@student.roc-nijmegen.nl>
2024-06-06 11:18:26 +0200

    wekrende register + wellicht inlog

--- html/pages/login-logic.php
+++ html/pages/login-logic.php
@@ -1,23 +1,23 @@
 <?php
     session_start();
     include 'conn.php';
-    if (empty($_POST['username']) || empty($_POST['password'])) {
+    if (empty($_POST['email']) || empty($_POST['password'])) {
         header("Location: login.php");
         exit();
     }
     
-    $username =  $_POST["username"];
+    $email =  $_POST["email"];
     $password =  $_POST["password"];
 
 
-    $stmt = $connection-> prepare("SELECT * FROM users WHERE username=:user AND password=:pass");
-    $stmt->execute(['user' => $username,'pass' => $password]);
+    $stmt = $connection-> prepare("SELECT * FROM users WHERE email=:email AND password=:pass");
+    $stmt->execute(['email' => $email,'pass' => $password]);
     $user = $stmt ->fetch();
  
     if (!$user){
         header("Location: login.php");
     } else {
-        $_SESSION["user"] = $username;
+        $_SESSION["email"] = $email;
         header("Location: dashboard.php");
     }
 ?>
