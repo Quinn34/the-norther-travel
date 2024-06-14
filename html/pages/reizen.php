b5a5a8fd228b697c471b297e1db341f95746a639
jopdejager <100798327+jopdejager@users.noreply.github.com>
2024-06-14 14:07:40 +0200

    rijzen page bijna klaar

--- html/pages/reizen.php
+++ html/pages/reizen.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="/css/main.css" >
+    <link rel="stylesheet" href="/css/jop.css">
 </head>
-<header>
-    <div class="nav">
+<body>
+<div class="nav">
         <div class="left-75">
             <div class="name">
                 <img src="/assets/north travel logo.png" class="kook">
@@ -27,3 +27,51 @@
         </div>
     </div>
     </div>
+    <div class="tussen-stuk"></div>
+
+        <?php
+     include 'conn.php';
+     
+     ?> 
+     <div class="container-reizen-page">
+
+        <?php
+
+          $stmt = $connection->query("SELECT products.name, products.id, products_Info.omschrijving,  products_Info.omschrijving_2,  products_Info.omschrijving_3
+          FROM products
+          INNER JOIN products_Info
+          ON products.id=products_Info.id");
+          while ($row = $stmt->fetch()) {
+            
+             
+            echo '<a class="container-reizen" href="product-info.php?id=' . $row['id'] . '">';
+
+        
+            //  echo '<a href="product-info.php?id=' . $row['id'] . '">';
+             echo '<div class="blah-box">';
+             echo '<img class="rijzen-fotos" src="../assets/chinatown1.jpeg" alt="">';
+                  echo '<div class="text-menu">';
+                  echo '<div class="container-name">';
+                //    echo '<a href="product-info.php?id=' . $row['id'] . '">';
+                  echo $row['name'] ."<br />\n";
+                  echo '</div>';
+                  echo '<div class="container-info">';
+                   echo $row['omschrijving'] ."<br/>\n";
+                   echo $row['omschrijving_2'] ."<br/>\n";
+                   echo $row['omschrijving_3'] ."<br/>\n";
+                   echo '</div>';
+                echo '<div class="container-button">';
+                //    echo ' <input class="buton-reizen" <a href="product-info.php?id=echo[id] $GET</a> type="submit" name="submit" value="Submit">';
+                   echo '</div>';
+     
+                   echo '</div>';
+                   echo '</div>';
+                   echo '</a>';
+
+          }          
+
+        ?>
+        </div>
+ </div>
+</body>
+</html>
